a84b945529d5fc7620ef4aabf1db5f3ff2cc5b00
andoyeon <56617914+andoyeon@users.noreply.github.com>
2020-01-09 18:19:46 +0900

    .

--- ch06/ex10_weight_compare.py
+++ ch06/ex10_weight_compare.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from ch06.ex02_sgd import Sgd
+from ch06.ex05_adam import Adam
 from common.multi_layer_net import MultiLayerNet
 from dataset.mnist import load_mnist
 
@@ -30,40 +31,41 @@ for key, type in weight_init_types.items():
 # MNIST train/test 데이터 로드
 (X_train, Y_train), (X_test, Y_test) = load_mnist(one_hot_label=True)
 
-iterations = 2_000  # 학습 횟수
-batch_size = 128    # 1번 학습에 사용할 샘플 개수(미니 배치)
-train_size = X_train.shape[0]
-optimizer = Sgd(learning_rate=0.01)   # 파라미터 최적화 알고리즘
-# optimizer를 변경하면서 테스트
+iterations = 2_000  # 학습 회수
+batch_size = 128  # 1번 학습에 사용할 샘플 개수(미니 배치)
+optimizer = Sgd(learning_rate=0.01)  # 파라미터 최적화 알고리즘
+# optimizer를 변경하면 테스트
+optimizer = Adam()
 
-# 2,000번 반복하면서
-for i in range(iterations):
+np.random.seed(109)
+for i in range(iterations):  # 2,000번 반복하면서
     # 미니 배치 샘플 랜덤 추출
-    batch_mask = np.random.choice(train_size, batch_size)
-    X_batch = X_train[batch_mask]
-    Y_batch = Y_train[batch_mask]
-    # 테스트 신경망 종류마다 반복
-    for key, value in neural_nets.items():
+    mask = np.random.choice(X_train.shape[0], batch_size)
+    X_batch = X_train[mask]
+    Y_batch = Y_train[mask]
+
+    for key, net in neural_nets.items():  # 테스트 신경망 종류마다 반복
         # gradient 계산
-        gradients = neural_nets[key].gradient(X_batch, Y_batch)
+        gradients = net.gradient(X_batch, Y_batch)
         # 파라미터(W, b) 업데이트
-        optimizer.update(neural_nets[key].params, gradients)
+        optimizer.update(net.params, gradients)
         # 손실(loss) 계산 -> 리스트 추가
-        loss = neural_nets[key].loss(X_batch, Y_batch)
+        loss = net.loss(X_batch, Y_batch)
         train_losses[key].append(loss)
     # 손실 일부 출력
-    if i % 300 == 0:
-        print(f'===== training #{i} =====')
-        for key in neural_nets:
-            print(key, ':', train_losses[key][-1])
+    if i % 100 == 0:
+        print(f'===== iteration #{i} =====')
+        for key, loss_list in train_losses.items():
+            print(key, ':', loss_list[-1])
 
 # x축-반복 횟수, y축-손실 그래프
 markers = {'std=0.01': 'o', 'Xavier': 's', 'He': 'D'}
 x = np.arange(iterations)
-for key in neural_nets.keys():
-    plt.plot(x, train_losses[key], marker=markers[key], markevery=100, label=key)
+for key, loss_list in train_losses.items():
+    plt.plot(x, loss_list, marker=markers[key], label=key)
+plt.title('Weight Init Compare')
 plt.xlabel('iterations')
-plt.ylabel('loss')
-plt.ylim(0, 2.5)
+plt.ylabel('Loss')
+# plt.ylim(0, 2.5)
 plt.legend()
 plt.show()
